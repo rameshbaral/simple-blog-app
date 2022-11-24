8e6a03e33de5c712332d8f558de556d2f0836ceb
Ramesh <baralramesh07@gmail.com>
2022-11-24 09:07:27 +0545

    Initialized the spring boot project

---
+++ src/main/java/com/ibriztech/springboot/SpringbootThymeleafBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.ibriztech.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafBlogAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafBlogAppApplication.class, args);
+    }
+
+}
