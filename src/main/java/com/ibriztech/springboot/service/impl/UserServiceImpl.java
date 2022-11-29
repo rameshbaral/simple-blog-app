6a5f221741ff1d9a051a77f9bdd5bba07c77df66
Ramesh <baralramesh07@gmail.com>
2022-11-29 15:30:23 +0545

    feat: add login and logout feature using spring security

--- src/main/java/com/ibriztech/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/ibriztech/springboot/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.ibriztech.springboot.entity.User;
 import com.ibriztech.springboot.repository.RoleRepository;
 import com.ibriztech.springboot.repository.UserRepository;
 import com.ibriztech.springboot.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -14,10 +15,15 @@ import java.util.Arrays;
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+
+    public UserServiceImpl(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                            PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -26,7 +32,7 @@ public class UserServiceImpl implements UserService {
         user.setName(registrationDto.getFirstName() + " " + registrationDto.getLastName());
         user.setEmail(registrationDto.getEmail());
         // use spring security to encrypt the password
-        user.setPassword(registrationDto.getPassword());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
         Role role = roleRepository.findByName("ROLE_GUEST");
         user.setRoles(Arrays.asList(role));
         userRepository.save(user);
